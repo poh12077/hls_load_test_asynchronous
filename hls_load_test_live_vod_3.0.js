cf94960887cfef769b47c834f3ba1ce19f3e73e9
yh <dudghk1207@gmail.com>
2022-02-18 19:48:48 +0900

    buffer

--- hls_load_test_live_vod_3.0.js
+++ hls_load_test_live_vod_3.0.js
@@ -1,8 +1,8 @@
 axios = require('axios');
 let fs = require('fs');
 
-//const url = 'http://192.168.0.124:1935/orange/nana/playlist.m3u8';   
-const url = 'http://192.168.0.124:1935/vod/mp4:sample.mp4/playlist.m3u8';
+//const url = 'http://192.168.0.124:1935/live/nana/playlist.m3u8';   //live   
+const url = 'http://192.168.0.124:1935/vod/mp4:sample.mp4/playlist.m3u8';   //vod
 
 let buffer=[];
 let ts_duration = 1000;
@@ -30,7 +30,7 @@ let parser_url = (url,buffer,i) =>
 }
 
 
-let request_m3u8 = (url, stop) =>
+let request_first_m3u8 = (url) =>
 {
   axios.get(url)
   .then( (response) => 
@@ -39,18 +39,42 @@ let request_m3u8 = (url, stop) =>
         //logger_request(response.status + " " + response.config.url, id);
         console.log( response.status + " " + response.config.url);
         buffer = response.data.split("\n");
-      
-        if( buffer[ buffer.length-2 ] == '#EXT-X-ENDLIST')
-       {
-            clearInterval(stop);
-       }
         parser_m3u8(buffer, url);
+  
   })
   .catch( (error) => {
       //logger_request(error, id);
     console.log(error);
   });
 }
+
+
+
+let request_second_m3u8 = (url) =>
+{
+  axios.get(url)
+  .then( (response) => 
+  {
+       // logger_time(id);
+        //logger_request(response.status + " " + response.config.url, id);
+        console.log( response.status + " " + response.config.url);
+        buffer = response.data.split("\n");
+        if( buffer[ buffer.length-2 ] == '#EXT-X-ENDLIST')
+       {
+           return 0;
+       }
+       else
+       {
+            startInterval(request_live_m3u8, url, ts_duration);
+       }
+  })
+  .catch( (error) => {
+      //logger_request(error, id);
+    console.log(error);
+  });
+}
+
+
 
 let parser_m3u8 = (buffer, url) => 
 {
@@ -59,12 +83,35 @@ let parser_m3u8 = (buffer, url) =>
         if ( buffer[i].slice(-4) == "m3u8" )
         {
                 url = parser_url(url,buffer,i);
-                startInterval(request_m3u8, url, ts_duration );
+               // startInterval(request_first_m3u8, url, ts_duration );
+               request_second_m3u8(url);
                 return 0;    //break ABR
         }
       
     }
   
+}
+
+let request_live_m3u8 = (url) =>
+{
+    let m3u8=[];
+    axios.get(url)
+  .then( (response) => 
+  {
+       // logger_time(id);
+        //logger_request(response.status + " " + response.config.url, id);
+        console.log( response.status + " " + response.config.url);
+        m3u8 = response.data.split("\n");
+      buffer = buffer.concat(m3u8);
+
+      buffer = new Set(buffer);
+      buffer = Array.from(buffer); 
+      
+  })
+  .catch( (error) => {
+      //logger_request(error, id);
+    console.log(error);
+  });
 }
 
 let startInterval = (callback, url, ts_duration ) => 
@@ -78,4 +125,4 @@ let startInterval = (callback, url, ts_duration ) =>
     callback(url,  stop);
 }
 
-request_m3u8(url);
+request_first_m3u8(url);
